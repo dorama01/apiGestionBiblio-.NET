442c1dc47821667219f240b8b0b1ce623f26c687
dorama01 <douae.maarad@gmail.com>
2024-12-31 21:54:30 +0100

    'push'

--- ApiChallenge/obj/Debug/net8.0/ApiChallenge.AssemblyInfo.cs
+++ ApiChallenge/obj/Debug/net8.0/ApiChallenge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda145cc96f2120874daa4acc68aa711616a24d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e692cfc04d32e92e9dfbbb44c8028b5db61db8f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
